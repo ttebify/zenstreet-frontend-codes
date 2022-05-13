563f7c1949faa863a9b8ed7b998f28c413b9a9ec
Justice Ekemezie <ttebify@gmail.com>
2022-05-13 02:53:10 +0100

    created how-it-works page

---
+++ src/pages/how-it-works.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Section from "../components/Layouts/Section";
+
+export default function HowItWorksPage() {
+  return <Section noPadding={false}>How it works</Section>;
+}
